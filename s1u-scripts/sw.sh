92a7a2fc8a27e19b7bc5264277cbfe212450d9a3
root <root@albacete.openstack>
2021-08-24 11:20:43 +0200

    updates

--- s1u-scripts/sw.sh
+++ s1u-scripts/sw.sh
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 sudo simple_switch --interface 0@ens5 --interface 1@ens4 $1.bmv2/$1.json &
-=======
-sudo simple_switch --interface 0@ens5 --interface 1@ens4  $1.bmv2/$1.json &
->>>>>>> 93f5241d2cfd898ba5c8b027d04aefdd6e863605
-
+#sudo simple_switch --interface 0@ens5 --interface 1@ens4  $1.bmv2/$1.json &
